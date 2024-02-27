19b33d707e3f07df6df49a638c3f4834f390e8bc
Matthew Douglass <matthew@globalworldwide.com>
2024-02-26 17:02:45 -0800

    Update to node.js v20, use groups for outputs

--- lib/index.ts
+++ lib/index.ts
@@ -3,9 +3,15 @@ import * as github from '@actions/github'
 import * as prettier from 'prettier'
 
 async function main() {
-  core.info(prettier.format(core.getInput('inputs'), { parser: 'json' }))
-  core.info(prettier.format(JSON.stringify(github.context), { parser: 'json' }))
-  core.info(prettier.format(JSON.stringify(process.env), { parser: 'json' }))
+  core.group('inputs', async () =>
+    core.info(await prettier.format(core.getInput('inputs'), { parser: 'json' })),
+  )
+  core.group('context', async () =>
+    core.info(await prettier.format(JSON.stringify(github.context), { parser: 'json' })),
+  )
+  core.group('environment', async () =>
+    core.info(await prettier.format(JSON.stringify(process.env), { parser: 'json' })),
+  )
 }
 
 main().catch((e) => core.setFailed(e.message))
